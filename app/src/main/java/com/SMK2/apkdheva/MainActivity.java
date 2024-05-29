5e462ac2ec7f4942f5dc6a8ad372d1722aa9f5bd
JD <dhevarizkika27@gmail.com>
2024-05-29 13:43:44 +0700

    Add some sound

--- app/src/main/java/com/SMK2/apkdheva/MainActivity.java
+++ app/src/main/java/com/SMK2/apkdheva/MainActivity.java
@@ -33,71 +33,79 @@ public class MainActivity extends AppCompatActivity {
         };
 
         // First Linear Layout Buttons
-        Button Button1 = findViewById(R.id.Btn1);
+        ImageView Button1 = findViewById(R.id.Button1);
         Button1.setTag(R.raw.salam);
         Button1.setOnClickListener(playSound);
 
-        Button Button2 = findViewById(R.id.Btn2);
+        ImageView Button2 = findViewById(R.id.Button2);
         Button2.setTag(R.raw.sus);
         Button2.setOnClickListener(playSound);
 
-        Button Button3 = findViewById(R.id.Btn3);
+        ImageView Button3 = findViewById(R.id.Button3);
         Button3.setTag(R.raw.yeaboy);
         Button3.setOnClickListener(playSound);
 
         // First Linear Layout Image Views
-        ImageView RButton = findViewById(R.id.Button1);
+        ImageView RButton = findViewById(R.id.Button4);
         RButton.setTag(R.raw.hellothere);
         RButton.setOnClickListener(playSound);
 
-        ImageView RButton1 = findViewById(R.id.Button2);
+        ImageView RButton1 = findViewById(R.id.Button5);
         RButton1.setTag(R.raw.horn);
         RButton1.setOnClickListener(playSound);
 
-        ImageView RButton2 = findViewById(R.id.Button3);
+        ImageView RButton2 = findViewById(R.id.Button6);
         RButton2.setTag(R.raw.booyah);
         RButton2.setOnClickListener(playSound);
 
-        ImageView RButton3 = findViewById(R.id.Button4);
+        ImageView RButton3 = findViewById(R.id.Button7);
         RButton3.setTag(R.raw.paimon);
         RButton3.setOnClickListener(playSound);
 
-        ImageView RButton4 = findViewById(R.id.Button5);
+        ImageView RButton4 = findViewById(R.id.Button8);
         RButton4.setTag(R.raw.spiderman);
         RButton4.setOnClickListener(playSound);
 
-        ImageView RButton5 = findViewById(R.id.Button6);
+        ImageView RButton5 = findViewById(R.id.Button9);
         RButton5.setTag(R.raw.bruh);
         RButton5.setOnClickListener(playSound);
 
-        ImageView Button69 = findViewById(R.id.BTN);
+        ImageView Button69 = findViewById(R.id.Button10);
         Button69.setTag(R.raw.americaya);
         Button69.setOnClickListener(playSound);
 
         // Second Linear Layout Buttons
-        ImageView Button4 = findViewById(R.id.Btn4);
+        ImageView Button4 = findViewById(R.id.Button11);
         Button4.setTag(R.raw.ustdmrh);
         Button4.setOnClickListener(playSound);
 
-        ImageView Button5 = findViewById(R.id.Btn5);
+        ImageView Button5 = findViewById(R.id.Button12);
         Button5.setTag(R.raw.wait);
         Button5.setOnClickListener(playSound);
 
-        ImageView Button6 = findViewById(R.id.Btn6);
+        ImageView Button6 = findViewById(R.id.Button13);
         Button6.setTag(R.raw.epep);
         Button6.setOnClickListener(playSound);
 
-        ImageView Button7 = findViewById(R.id.Btn7);
+        ImageView Button7 = findViewById(R.id.Button14);
         Button7.setTag(R.raw.ustdmrh);
         Button7.setOnClickListener(playSound);
 
-        ImageView Button8 = findViewById(R.id.Btn8);
+        ImageView Button8 = findViewById(R.id.Button15);
         Button8.setTag(R.raw.wait);
         Button8.setOnClickListener(playSound);
 
-        ImageView Button9 = findViewById(R.id.Btn9);
-        Button9.setTag(R.raw.epep);
+        ImageView Button9 = findViewById(R.id.Button16);
+        Button9.setTag(R.raw.ack);
         Button9.setOnClickListener(playSound);
+
+        ImageView Button10 = findViewById(R.id.Button17);
+        Button10.setTag(R.raw.shock);
+        Button10.setOnClickListener(playSound);
+
+        ImageView Button11 = findViewById(R.id.Button18);
+        Button11.setTag(R.raw.yntkts);
+        Button11.setOnClickListener(playSound);
 
 
     }
